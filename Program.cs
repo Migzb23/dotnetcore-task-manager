cf165644c000c51e760579bb6055b219de41e8c7
Migzb23 <97329456+Migzb23@users.noreply.github.com>
2026-07-08 10:48:33 +0800

    Fix Program.cs - use WebApplication.CreateBuilder instead of WebApplicationBuilder.CreateBuilder

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using TaskManager.Services;
 
-var builder = WebApplicationBuilder.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
